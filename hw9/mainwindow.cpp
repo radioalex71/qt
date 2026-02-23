4e824f76a7498fc8af8f45cb4f90634285456f6c
Alexey Ryabinkin <a.ryabinkin71@gmail.com>
2026-02-23 15:37:40 +0300

    lesson_09

---
+++ hw9/mainwindow.cpp
@@ -0,0 +1,87 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    udpWorker = new UDPworker(this);
+    udpWorker->InitSocket(12345);
+
+    connect(udpWorker, &UDPworker::sig_sendTimeToGUI, this, &MainWindow::DisplayTime);
+
+    udpWorkerSendDatagram = new UDPworker(this);
+    udpWorkerSendDatagram->InitSocket(123456);
+    connect(udpWorkerSendDatagram, &UDPworker::sig_sendInputTextToGUI, this, &MainWindow::DisplayInputText);
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, [&]{
+
+        QDateTime dateTime = QDateTime::currentDateTime();
+
+        QByteArray dataToSend;
+        QDataStream outStr(&dataToSend, QIODevice::WriteOnly);
+
+        outStr << dateTime;
+
+        udpWorker->SendDatagram(dataToSend);
+        timer->start(TIMER_DELAY);
+
+    });
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pb_start_clicked()
+{
+    timer->start(TIMER_DELAY);
+}
+
+
+void MainWindow::DisplayTime(QDateTime data)
+{
+    counterPck++;
+    if(counterPck % 20 == 0){
+        ui->te_result->clear();
+    }
+
+    ui->te_result->append("Текущее время: " + data.toString() + ". " +
+                          "Принято пакетов " + QString::number(counterPck));
+
+
+}
+
+void MainWindow::DisplayInputText(QString data, QString address, int port)
+{
+
+    ui->te_result->append("Принято сообщение от " + address + ":" +
+                          QString::number(port) + ", размер сообщения (байт): " +
+                          QString::number(data.size()));
+}
+
+
+void MainWindow::on_pb_stop_clicked()
+{
+    timer->stop();
+}
+
+void MainWindow::on_pb_sendDatagram_clicked()
+{
+
+    QString dateInputText = ui->le_inputText->text();
+
+    QByteArray dataToSend;
+    QDataStream outStr(&dataToSend, QIODevice::WriteOnly);
+
+    outStr << dateInputText;
+
+    udpWorkerSendDatagram->SendDatagram(dataToSend);
+}
+
---
+++ hw9/udpworker.cpp
@@ -0,0 +1,70 @@
+#include "udpworker.h"
+
+UDPworker::UDPworker(QObject *parent) : QObject(parent) {}
+
+/*!
+ * @brief Метод инициализирует UDP сервер
+ */
+void UDPworker::InitSocket(int bindPort)
+{
+    bindPort_ = bindPort;
+    serviceUdpSocket = new QUdpSocket(this);
+    /*
+     * Соединяем присваиваем адрес и порт серверу и соединяем функцию
+     * обработчик принятых пакетов с сокетом
+     */
+    serviceUdpSocket->bind(QHostAddress::LocalHost, bindPort_);
+
+    connect(serviceUdpSocket, &QUdpSocket::readyRead, this, &UDPworker::readPendingDatagrams);
+
+}
+
+/*!
+ * @brief Метод осуществляет обработку принятой датаграммы
+ */
+void UDPworker::ReadDatagram(QNetworkDatagram datagram)
+{
+
+    QByteArray data;
+    data = datagram.data();
+
+    QDataStream inStr(&data, QIODevice::ReadOnly);
+    if (bindPort_ == 12345)
+    {
+        QDateTime dateTime;
+        inStr >> dateTime;
+        emit sig_sendTimeToGUI(dateTime);
+    }
+    else if(bindPort_ == 123456)
+    {
+        QString dateInputText;
+        inStr >> dateInputText;
+        QString localAddress = serviceUdpSocket->localAddress().toString();
+        emit sig_sendInputTextToGUI(dateInputText, localAddress, bindPort_);
+    }
+}
+/*!
+ * @brief Метод осуществляет опередачу датаграммы
+ */
+void UDPworker::SendDatagram(QByteArray data)
+{
+    /*
+     *  Отправляем данные на localhost и задефайненный порт
+     */
+    serviceUdpSocket->writeDatagram(data, QHostAddress::LocalHost, bindPort_);
+}
+
+/*!
+ * @brief Метод осуществляет чтение датаграм из сокета
+ */
+void UDPworker::readPendingDatagrams( void )
+{
+    /*
+     *  Производим чтение принятых датаграмм
+     */
+    while(serviceUdpSocket->hasPendingDatagrams()){
+            QNetworkDatagram datagram = serviceUdpSocket->receiveDatagram();
+            ReadDatagram(datagram);
+    }
+
+}
